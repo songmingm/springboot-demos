1df61c701ff66b14e4bdb6abdda1941025a2111a
song2m <mmsong@yeah.net>
2023-02-18 23:05:27 +0800

    😁websocket demo

---
+++ webSocket/webSocket/src/main/java/org/example/websocket/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package org.example.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 开启WebSocket支持
+ *
+ * @author song2m
+ * @since 2023/2/18 22:18
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
--- websocket/websocket/src/main/java/org/example/websocket/App.java
+++
@@ -1,9 +0,0 @@
-package org.example.websocket;
-
-/**
- * 启动类
- * @author song2m
- * @since 2023/2/18 22:09
- */
-public class App {
-}
